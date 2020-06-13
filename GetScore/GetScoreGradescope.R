dbe2b97f8896db4968c9b224ce6200e088536ec0
Dat Duong <datd1988@gmail.com>
2020-06-12 21:49:53 -0700

    up

--- GetScore/GetScoreGradescope.R
+++ GetScore/GetScoreGradescope.R
@@ -3,9 +3,12 @@
 ##! stepik
 setwd('C:/Users/dat/Documents/CS122Spring2020Record')
 
-stepik = read.csv('course-65953-grade-book.csv')
-stepik [, 2:3] = apply(stepik[, 2:3], 2, toupper)
+for (chap in c(3,5,9)) { 
 
+  stepik = read.csv('course-65953-grade-book.csv')
+  stepik [, 2:3] = apply(stepik[, 2:3], 2, toupper)
+
+if (chap == 3) {
 ##! question chapter 3
 questions = "The String Reconstruction Problem	
 String Reconstruction as a Walk in the Overlap Graph	
@@ -14,60 +17,69 @@ Walking in the de Bruijn Graph
 From Euler.s Theorem to an Algorithm for Finding Eulerian Cycles	
 Assembling Genomes from Read.Pairs	
 Epilogue. Genome Assembly Faces Real Sequencing Data"
-
-##! question chapter 5
-# questions = "An Introduction to Dynamic Programming. The Change Problem	
-# The Manhattan Tourist Problem Revisited	
-# Backtracking in the Alignment Graph	
-# From Global to Local Alignment	
-# The Changing Faces of Sequence Alignment	
-# Penalizing Insertions and Deletions in Sequence Alignment	
-# Space.Efficient Sequence Alignment	
-# Epilogue. Multiple Sequence Alignment"
-
-##! question chapter 9
-# questions = "Herding Patterns into a Trie	
-# Suffix Trees	
-# Suffix Arrays	
-# The Burrows.Wheeler Transform	
-# The First.Last Property and Burrows.Wheeler Inversion	
-# Pattern Matching with the Burrows.Wheeler Transform	
-# Speeding Up Burrows.Wheeler Pattern Matching	
-# Burrows and Wheeler Set Up Checkpoints	
-# Epilogue. Mismatch.Tolerant Read Mapping"
-
-
-questions = strsplit(questions,'\n')[[1]]
-questions = gsub(" ",".",questions)
-
-coln = names(stepik)
-coli = c()
-for (q in questions){
-  print (q)
-  coli = c( coli, grep(q,coln) )
-  print (coli)
 }
 
-score = rowSums ( stepik[,coli] ) 
+if (chap == 5) {
+##! question chapter 5
+questions = "An Introduction to Dynamic Programming. The Change Problem	
+The Manhattan Tourist Problem Revisited	
+Backtracking in the Alignment Graph	
+From Global to Local Alignment	
+The Changing Faces of Sequence Alignment	
+Penalizing Insertions and Deletions in Sequence Alignment	
+Space.Efficient Sequence Alignment	
+Epilogue. Multiple Sequence Alignment"
+}
 
-temp = as.numeric( c('5', '5', '10', '15', '5', '15', '5', '10', '10', '10', '10') )
-sum(temp)
-temp = as.numeric( c('10', '10', '10', '10', '15', '10', '5', '10', '10', '10', '10', '15', '10')) # 5
-sum(temp)
-temp = as.numeric( c('10', '10', '15', '10', '10', '10', '10', '10', '5', '10', '10', '15', '15'))
-sum(temp) #! double check
+if (chap == 9) {
+##! question chapter 9
+questions = "Herding Patterns into a Trie	
+Suffix Trees	
+Suffix Arrays	
+The Burrows.Wheeler Transform	
+The First.Last Property and Burrows.Wheeler Inversion	
+Pattern Matching with the Burrows.Wheeler Transform	
+Speeding Up Burrows.Wheeler Pattern Matching	
+Burrows and Wheeler Set Up Checkpoints	
+Epilogue. Mismatch.Tolerant Read Mapping"
+}
 
-Q = cbind ( stepik [, 2:3], score)
-names(Q) = c( "Last.Name", "First.Name", "score" )
-# Qx = merge(Q,midterm,by=c("First.Name", "Last.Name"))
-Qx = Q 
-Qx = Qx [ order(Qx$score,decreasing=T), ]
+  questions = strsplit(questions,'\n')[[1]]
+  questions = gsub(" ",".",questions)
 
-Qx = Qx[ ! duplicated(Qx$First.Name), ]
-Qx[ which (Qx[,1]==""), 1 ] = 'none'
-Qx[ which (Qx[,2]==""), 2 ] = 'none'
+  coln = names(stepik)
+  coli = c()
+  for (q in questions){
+    print (q)
+    coli = c( coli, grep(q,coln) )
+    print (coli)
+  }
 
-write.table(Qx,file='StepikChap3Score.csv',row.names=F,quote=F,na='none',sep='\t')
+  score = rowSums ( stepik[,coli] ) 
+
+  # temp = as.numeric( c('5', '5', '10', '15', '5', '15', '5', '10', '10', '10', '10') )
+  # sum(temp)
+  # temp = as.numeric( c('10', '10', '10', '10', '15', '10', '5', '10', '10', '10', '10', '15', '10')) # 5
+  # sum(temp)
+  # temp = as.numeric( c('10', '10', '15', '10', '10', '10', '10', '10', '5', '10', '10', '15', '15'))
+  # sum(temp) #! double check
+
+  Q = cbind ( stepik [, 2:3], score)
+  names(Q) = c( "Last.Name", "First.Name", "score" )
+  # Qx = merge(Q,midterm,by=c("First.Name", "Last.Name"))
+  Qx = Q 
+  Qx = Qx [ order(Qx$score,decreasing=T), ]
+
+  Qx = Qx[ ! duplicated(Qx$First.Name), ]
+  Qx[ which (Qx[,1]==""), 1 ] = 'none' ## so stupid
+  Qx[ which (Qx[,2]==""), 2 ] = 'none'
+  Qx[ which (Qx[,3]==""), 3 ] = 'none'
+
+  write.table(Qx,file=paste0('StepikChap',chap,'Score.csv'),row.names=F,quote=F,na='none',sep='\t')
+
+}
+
+
 
 ####
 
